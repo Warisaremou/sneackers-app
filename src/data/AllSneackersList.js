bfc248ae1e6f25208f5a4b885b0f49d69080051a
Waris Aremou <warisaremou2003@gmail.com>
2022-11-03 02:57:14 +0100

    Ajout d'un tableau contenant la liste des produits

---
+++ src/data/AllSneackersList.js
@@ -0,0 +1,35 @@
+import product1 from "../pictures/image-product-1.jpg";
+import product1Thumbnail from "../pictures/image-product-1-thumbnail.jpg";
+import product2 from "../pictures/image-product-2.jpg";
+import product2Thumbnail from "../pictures/image-product-2-thumbnail.jpg";
+import product3 from "../pictures/image-product-3.jpg";
+import product3Thumbnail from "../pictures/image-product-3-thumbnail.jpg";
+import product4 from "../pictures/image-product-4.jpg";
+import product4Thumbnail from "../pictures/image-product-4-thumbnail.jpg";
+
+export const AllSneackersList = [
+  {
+    id: "1",
+    name: "product1",
+    thumbnail: product1Thumbnail,
+    cover: product1,
+  },
+  {
+    id: "2",
+    name: "product2",
+    thumbnail: product2Thumbnail,
+    cover: product2,
+  },
+  {
+    id: "3",
+    name: "product3",
+    thumbnail: product3Thumbnail,
+    cover: product3,
+  },
+  {
+    id: "4",
+    name: "product4",
+    thumbnail: product4Thumbnail,
+    cover: product4,
+  },
+];
