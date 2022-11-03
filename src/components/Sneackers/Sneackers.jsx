c9c5dcadc6b7c02ce0de10513855a8db070cce8f
Waris Aremou <warisaremou2003@gmail.com>
2022-11-03 03:08:22 +0100

    modification du style css

--- src/components/Sneackers/Sneackers.jsx
+++ src/components/Sneackers/Sneackers.jsx
@@ -4,7 +4,7 @@ import SneackersList from "../SneackersLIst/SneackersList";
 
 function Sneackers() {
   return (
-    <div className="flex gap-10 flex-col lg:flex-row justify-between mt-12">
+    <div className="flex gap-24 flex-col lg:flex-row justify-between items-center my-12">
       <SneackersList />
       <SneackersInfo />
     </div>
