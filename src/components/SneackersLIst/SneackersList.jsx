112cdb3ad4407b2a2c79d4fce52435d0a6a73cb1
Waris Aremou <warisaremou2003@gmail.com>
2022-11-03 03:07:01 +0100

    Récupération des données passées en prop depuis Thumbnail et ajout d'un state pour changer le cover en fontion du thumbnail cliqué

--- src/components/SneackersLIst/SneackersList.jsx
+++ src/components/SneackersLIst/SneackersList.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import sneacker1 from "../../pictures/image-product-1.jpg"
+import sneacker1 from "../../pictures/image-product-1.jpg";
+import Thumbnail from "./Thumbnail";
+import { AllSneackersList } from "../../data/AllSneackersList";
+import { useState } from "react";
 
 function SneackersList() {
+  const [isPictureActive, setIsPictureActive] = useState("");
+
   return (
     <div>
-      <img src={sneacker1} alt="" className="w-96 rounded-xl" />
+      <img src={isPictureActive ? isPictureActive : sneacker1} alt="" className="w-fit rounded-xl" />
+      <div className="grid grid-cols-4 gap-4 lg:gap-6 mt-6">
+        {AllSneackersList.map((sneacker) => (
+          <Thumbnail
+            key={sneacker.id}
+            name={sneacker.name}
+            thumbnailCover={sneacker.thumbnail}
+            cover={sneacker.cover}
+            isPictureActive={isPictureActive}
+            setIsPictureActive={setIsPictureActive}
+          />
+        ))}
+      </div>
     </div>
   );
 }
