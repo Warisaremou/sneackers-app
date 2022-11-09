9c966bb98e02fa373b401a2324219c10227f4941
Waris Aremou <warisaremou2003@gmail.com>
2022-11-09 21:56:53 +0100

    Importation du composant Layout et du la liste des sneackers, Création du state openMenu passé en props aux composants NavBar et Layout et du state sneackerDetails passé en props au composant Sneackers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
 import NavBar from "./NavBar/NavBar";
 import Sneackers from "./Sneackers/Sneackers";
+import Layout from "./Layout";
+import { useState } from "react";
+import { AllSneackersList } from "../data/AllSneackersList";
 
 function App() {
+  const [openMenu, setIsOpen] = useState(false);
+  const [sneackerDetails, setSneackerDetails] = useState(AllSneackersList);
+
   return (
-    <div className="px-6 md:px-8 lg:container font-poppins">
-      <NavBar />
-      <Sneackers />
-    </div>
+    <>
+      <div className="px-6 md:px-8 lg:container font-poppins">
+        <NavBar openMenu={openMenu} setIsOpen={setIsOpen} />
+        <Sneackers
+          sneackerDetails={sneackerDetails}
+          setSneackerDetails={setSneackerDetails}
+        />
+      </div>
+      <Layout openMenu={openMenu} setIsOpen={setIsOpen} />
+    </>
   );
 }
 
