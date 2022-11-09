40c7d5c08c49c3f65fec517356e2781b3765741c
Waris Aremou <warisaremou2003@gmail.com>
2022-11-09 21:37:44 +0100

    Ajout du responsive et importation de useState

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import Cart from "../Cart/Cart";
 import logo from "../../pictures/logo.svg";
 import userProfil from "../../pictures/image-avatar.png";
+import { useState } from "react";
 import { HiOutlineMenu } from "react-icons/hi";
+import { IoMdClose } from "react-icons/io";
 
-function NavBar() {
+function NavBar({ openMenu, setIsOpen }) {
+
   return (
-    <nav className="navbar ">
-      <HiOutlineMenu className="text-2xl block md:hidden mt-1 cursor-pointer" />
-      <ul className="navbar-nav">
-        <div className="logo">
-          <img src={logo} alt="Sneackers-logo" />
-        </div>
+    <nav className="navbar">
+      <HiOutlineMenu className="text-gray-500 text-2xl md:hidden mt-1 cursor-pointer" onClick={() => setIsOpen(true)} />
+      <div className="logo">
+        <img src={logo} alt="Sneackers-logo" />
+      </div>
+      <ul className={`flex flex-col md:flex-row fixed z-50 md:relative bg-white md:bg-transparent top-0 bottom-0 h-full md:h-auto w-60 md:w-auto py-12 pl-6 duration-500 ${openMenu ? 'left-0' : '-left-full md:left-0'}`}>
+        <IoMdClose className="text-3xl text-gray-500 md:hidden cursor-pointer" onClick={() => setIsOpen(false)} />
         <li className="nav-item">
           <a className="nav-link" href="/">
             Collections
