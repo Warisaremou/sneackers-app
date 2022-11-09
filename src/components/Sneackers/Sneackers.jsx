3bdd075d3654660dfdd2b6a69997cc8254f51944
Waris Aremou <warisaremou2003@gmail.com>
2022-11-09 21:43:29 +0100

    Ajout des props sneackerDetails et setSneackerDetails aux composants Sneackers, SneackersList et SneackersInfo et création du state cart

--- src/components/Sneackers/Sneackers.jsx
+++ src/components/Sneackers/Sneackers.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import SneackersInfo from "../SneackersInfo/SneackersInfo";
 import SneackersList from "../SneackersLIst/SneackersList";
+import { useState } from "react";
 
-function Sneackers() {
+function Sneackers({ sneackerDetails, setSneackerDetails }) {
+  const [isPictureActive, setIsPictureActive] = useState("");
+
+  const [cart, updateCart] = useState([]);
+
   return (
     <div className="flex gap-24 flex-col lg:flex-row justify-between items-center my-12">
-      <SneackersList />
-      <SneackersInfo />
+      <SneackersList
+        isPictureActive={isPictureActive}
+        setIsPictureActive={setIsPictureActive}
+        sneackerDetails={sneackerDetails}
+        setSneackerDetails={setSneackerDetails}
+      />
+      <SneackersInfo
+        sisPictureActive={isPictureActive}
+        setIsPictureActive={setIsPictureActive}
+        sneackerDetails={sneackerDetails}
+        setSneackerDetails={setSneackerDetails}
+      />
     </div>
   );
 }
